6d11f8f7683751216e079db03b301ae1e4b18862
Rafael da Silva <rs.rafaeldasilva@gmail.com>
2024-09-01 04:18:49 -0300

    update backend

--- deployments/vanilla/terraform/backend.tf
+++ deployments/vanilla/terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-381491993605-uw2"
-    key    = "state/mlops_test/kubeflow/terraform.tfstate"
+    key    = "state/mlops_test/kubeflow/vanilla/terraform.tfstate"
     region = "us-west-2"
   }
 }
